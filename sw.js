f7c86a47231ca4a28bf28ba64de61922662792a2
pablo0593 <pablito.alderete21@gmail.com>
2019-12-07 00:56:00 -0300

    tateti2

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener("intall",evt => {console.log("El service worked se instalo
 
 
 //Activar el service worker.
-self.addEventListener("activate",evt => {console.log("El service workerse activo.");})
+self.addEventListener("activate",evt => {console.log("El service workerse activo.");});
 
 //Eventos Fetch (fetch request o pedido de busqueda).
-
+self.addEventListener("fetch", evt=> {console.log("Se atrapo ese evento: ",evt);});
