5c0a4db1c7d06bcc00d8aecfa9784981cc4c7625
pablo0593 <pablito.alderete21@gmail.com>
2019-12-07 01:23:10 -0300

    arreglado

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ if('serviceWorker' in navigator)
 	{
 		console.log("Acepta service worker");
 		navigator.serviceWorker.register('sw.js')
-		.then((reg) => console.log("Service Worker Registrado",reg)) /*Si el registro sucede, entoces realiza esto.*/
-		.catch((err) => console.log("Service Worker No Registrado",err)); /*Sino, atrapa el error e informa.*/
+		.then((reg) =>  console.log("Service Worker Registrado",reg))
+		.catch((err) => console.log("Service Worker No Registrado",err));
 	}
