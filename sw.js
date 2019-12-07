d9fb0c85b333fae76f0339c212b0a0a4257008af
pablo0593 <pablito.alderete21@gmail.com>
2019-12-07 00:31:01 -0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,9 @@
+//Instalar el service worker.
+self.addEventListener("intall",evt => {console.log("El service worked se instalo.")});
+
+
+//Activar el service worker.
+
+
+//Eventos Fetch (fetch request o pedido de busqueda).
+
